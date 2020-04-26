392cec0bdb6de222699fe9e457fe01bb23787e45
nakamura <cousaka0623@gmail.com>
2020-04-26 13:24:45 +0000

    Edit tasklist 20200426_4

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,7 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :set_task, only: [:show, :edit, :update]
   before_action :correct_user, only: [:show, :edit, :update, :destroy]
-  # before_action :current_user_task_id, only: [:show, :edit, :update]
-  
+
   def index
     @tasks = current_user.tasks.order(id: :desc).page(params[:page]).per(3)
   end
@@ -45,7 +43,6 @@ class TasksController < ApplicationController
     @task.destroy
     
     flash[:success] = 'Taskは正常に削除されました。'
-    # redirect_back(fallback_location: root_path)
     redirect_to @task
   end
   
@@ -65,11 +62,5 @@ class TasksController < ApplicationController
        redirect_to root_url
      end
    end
-   
-  # def current_user_task_id
-  #   current_user_task_id = current_user.tasks.find_by(id: params[:id])
-  #   unless current_user_task_id == @task
-  #     redirect_to root_url
-  #   end
-  # end
+
 end
